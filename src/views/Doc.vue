f45cad75f0c6a1bae007d286bfbcc7bdf6f67e54
TacitusPie <kakmsaikou@gmail.com>
2022-09-30 10:03:29 +0800

    封装 TopNav 组件

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>
-    <div class="top-nav">
-      <div class="logo"></div>
-      <div class="menu"></div>
-    </div>
-    <div class="content">
-      <aside>边栏</aside>
-      <main>主内容</main>
-    </div>
+  <TopNav/>
+  <div class="content">
+    <aside>边栏</aside>
+    <main>主内容</main>
   </div>
 </template>
+
+<script lang="ts">
+  import TopNav from '../components/TopNav.vue';
+
+  export default {
+    components: {TopNav}
+  };
+</script>
