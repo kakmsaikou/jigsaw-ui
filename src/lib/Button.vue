b0b9b75a09a7bc236fb322ac2e2502c8edfead1d
TacitusPie <kakmsaikou@gmail.com>
2022-09-30 21:11:03 +0800

    添加 theme 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,17 @@
 <template>
-  <button>
+  <button class="jgso-button"
+          :class="`theme-${theme}`">
     <slot/>
   </button>
 </template>
+
+<script lang="ts">
+  export default {
+    props: {
+      theme: {
+        type: String,
+        default: 'button'
+      }
+    }
+  };
+</script>
