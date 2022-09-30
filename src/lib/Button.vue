f3d41f8c60d47ee35d0cc2a4b54ce7e08d709b3e
TacitusPie <kakmsaikou@gmail.com>
2022-09-30 21:42:38 +0800

    支持 theme 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -53,5 +53,25 @@
     &::-moz-focus-inner {
       border: 0
     }
+
+    &.jgso-theme-link {
+      border-color: transparent;
+      box-shadow: none;
+      color: $blue;
+
+      &:hover, &:focus {
+        color: lighten($blue, 10%)
+      }
+    }
+
+    &.jgso-theme-text {
+      border-color: transparent;
+      box-shadow: none;
+      color: inherit;
+
+      &:hover, &:focus {
+        background: darken(white, 5%);
+      }
+    }
   }
 </style>
