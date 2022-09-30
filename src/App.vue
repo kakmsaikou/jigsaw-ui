146f905d380eaab269ca6a118dae8db414f81a1f
TacitusPie <kakmsaikou@gmail.com>
2022-09-30 08:41:00 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Hi</div>
+  <Tacitus/>
+</template>
+
+<script>
+  import Tacitus from './components/Tacitus.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      Tacitus
+    }
+  }
+</script>
---
+++ src/components/Tacitus.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    我的第一个组件
+  </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
