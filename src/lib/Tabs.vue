f5c6ea552abe576e51e75e7cdc88f6b7fbc75f8f
TacitusPie <kakmsaikou@gmail.com>
2022-10-01 15:39:56 +0800

    Tabs css 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,12 @@
 <template>
-  Tabs 组件
-  <div v-for="(t, index) in titles" :key="index">{{t}}</div>
-  <component v-for="c in components" :is="c"/>
+  <div class="jgso-tabs">
+    <div class="jgso-tabs-nav">
+      <div class="jgso-tabs-nav-item" v-for="(t, index) in titles" :key="index">{{ t }}</div>
+    </div>
+    <div class="jgso-tabs-content">
+      <component class="jgso-tabs-content-item" v-for="c in components" :is="c"/>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -23,3 +28,35 @@
 
   };
 </script>
+
+<style lang="scss">
+  $blue: #40a9ff;
+  $color: #333;
+  $border-color: #d9d9d9;
+
+  .jgso-tabs {
+    &-nav {
+      display: flex;
+      color: $color;
+      border-bottom: 1px solid $border-color;
+
+      &-item {
+        padding: 8px 0;
+        margin: 0 16px;
+        cursor: pointer;
+
+        &:first-child {
+          margin-left: 0;
+        }
+
+        &.selected {
+          color: $blue;
+        }
+      }
+    }
+
+    &-content {
+      padding: 8px 0;
+    }
+  }
+</style>
