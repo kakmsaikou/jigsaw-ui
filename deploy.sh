b23c3d967e448ec5cd90f82093b54545e4c3862c
TacitusPie <kakmsaikou@gmail.com>
2022-10-07 15:27:39 +0800

    添加一键部署

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@ rm -rf dist &&
 yarn build &&
 cd dist &&
 git init &&
+git checkout -b gh-pages &&
 git add . &&
 git commit -m 'update' &&
-git remote add origin git@gitee.com:kakmsaikou/jigsaw-ui-webside.git &&
-git push -f -u origin "master" &&
+git remote add origin git@github.com:kakmsaikou/jigsaw-ui.git &&
+git push -f -u origin "gh-pages" &&
 cd -
